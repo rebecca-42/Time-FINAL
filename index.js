3397d1fce268adf74d5237a02197109909d489e0
Rebecca Sweigart <143544823+rebecca-42@users.noreply.github.com>
2024-03-05 16:07:59 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+function calculateTimeElapsed() {
+    const dobInput = document.getElementById('dob').value;
+    const timeInput = document.getElementById('time').value;
+
+    const datetimeString = dobInput + " " + timeInput;
+
+    const momentObject = moment(datetimeString, "YYYY-MM-DD HH:mm");
+    const totalMinutes = moment().diff(momentObject, 'minutes');
+
+    const years = moment().diff(momentObject, 'years');
+    const months = moment().diff(momentObject, 'months') % 12;
+    const weeks = moment().diff(momentObject, 'weeks') % 4;
+    const days = moment().diff(momentObject, 'days') % 7;
+    const hours = moment().diff(momentObject, 'hours') % 24;
+    const minutes = moment().diff(momentObject, 'minutes') % 60;
+    
+    // AGE
+    document.getElementById('age').innerText = `${years} years, ${months} months, ${weeks} weeks, ${days} days, ${hours} hours, and ${minutes} minutes old and counting...`;
+
+    // HEARTBEATS
+    const averageHeartbeats = totalMinutes * 80;
+    const formattedHeartbeats = averageHeartbeats.toLocaleString();
+    // 80 bpm is average
+    document.getElementById('heartbeats').innerText = `${formattedHeartbeats} heartbeats and counting...`;
+
+    // BREATHS
+    const averageBreaths = totalMinutes * 15;
+    const formattedBreaths = averageBreaths.toLocaleString();
+    // 15 breaths per minute is average
+    document.getElementById('breaths').innerText = `${formattedBreaths} breaths and counting...`;
+}
