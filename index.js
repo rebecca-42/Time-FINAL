d22373eedd11bf0f1cc05835de2c8d7d25f64080
Rebecca Sweigart <143544823+rebecca-42@users.noreply.github.com>
2024-03-11 16:21:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 function calculateTimeElapsed() {
     const dobInput = document.getElementById('dob').value;
     const timeInput = document.getElementById('time').value;
-
-    const userTimeZone = prompt('Please enter your time zone (e.g., America/New_York):');
-    if (!userTimeZone) {
-        alert('Invalid time zone. Please refresh the page and try again.');
-        return;
-    }
     
     const datetimeString = dobInput + " " + timeInput;
     const momentObject = moment.tz(datetimeString, "YYYY-MM-DD HH:mm", userTimeZone);
